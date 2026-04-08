35b73cf1223e49d078bbdc6f55d66c248f596da8
Roderick Carl Satina <carlsatina@gmail.com>
2026-04-08 20:30:56 +0800

    Remove migrations.

--- prisma/migrations/20260408120156_fleet/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Vehicle" DROP CONSTRAINT "Vehicle_driverId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
